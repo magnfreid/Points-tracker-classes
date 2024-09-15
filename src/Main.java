8f20fe369d53684a2b55a5e4db860b756ad6c982
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-09-15 23:18:30 +0200

    Final for,

--- src/Main.java
+++ src/Main.java
@@ -7,39 +7,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-        ArrayList<Player> playerList = new ArrayList<>();
+        ArrayList<Player> players = new ArrayList<>();
         //Array med alla menyer, för att lättare kunna lägga till och ta bort
         String[] menuItems = {
-                "1. Add score",
-                "2. Show all scores",
+                "1. Add player",
+                "2. Show all players",
                 "3. Show highest score",
                 "4. Show average score",
                 "5. Clear list",
                 "6. Exit"};
         while (running) {
-            int playerCount = playerList.size();
+            int playerCount = players.size();
             printMenu(playerCount, menuItems);
             String userInput = scanner.nextLine();
             if (checkInput(userInput, menuItems)) {
                 switch (userInput) {
                     case "1": {
-                        createPlayer(playerList);
+                        addPlayer(players);
                         break;
                     }
                     case "2": {
-                        printList(playerList);
+                       printList(players);
                         break;
                     }
                     case "3": {
-                        printHighScore(playerList);
+                       printHighScore(players);
                         break;
                     }
                     case "4": {
-                        printAverageScore(playerList);
+                       printAverageScore(players);
                         break;
                     }
                     case "5": {
-                        clearScoreList(playerList);
+                       clearScoreList(players);
                         break;
                     }
                     case "6": {
@@ -55,55 +55,57 @@ public class Main {
         }
     }
 
-    //TODO adjust to new Player class
-    private static void printList(ArrayList<Integer> scoresList) {
-        if (!scoresList.isEmpty()) {
-            System.out.println("All scores:");
-            for (int score : scoresList) {
-                System.out.println(score);
+    
+    private static void printList(ArrayList<Player> players) {
+        if (!players.isEmpty()) {
+            System.out.println("All players:");
+            for (Player player : players) {
+                System.out.println(player);
             }
         } else {
-            System.out.println("Score list is empty...");
+            System.out.println("List of players is empty...");
         }
     }
 
     //TODO adjust to new Player class
-    private static void clearScoreList(ArrayList<Integer> scoresList) {
-        scoresList.clear();
+    private static void clearScoreList(ArrayList<Player> players) {
+        players.clear();
         System.out.println("List cleared...");
     }
 
     //TODO adjust to new Player class
-    private static void printAverageScore(ArrayList<Integer> scoresList) {
-        if (!scoresList.isEmpty()) {
+    private static void printAverageScore(ArrayList<Player> players) {
+        if (!players.isEmpty()) {
             int totalScore = 0;
-            for (int score : scoresList) {
-                totalScore = totalScore + score;
+            for (Player player : players) {
+                totalScore += player.getScore();
             }
-            double averageScore = (double) totalScore / scoresList.size();
-            System.out.println("The average score out of a total of " + scoresList.size() + " scores is: " + averageScore);
+            double averageScore = (double) totalScore / players.size();
+            System.out.println("The average score out of a total of " + players.size() + " players is: " + averageScore);
         } else {
-            System.out.println("Score list is empty...");
+            System.out.println("List of players is empty...");
         }
     }
 
     //TODO adjust to new Player class
-    private static void printHighScore(ArrayList<Integer> scoresList) {
+    private static void printHighScore(ArrayList<Player> players) {
         int highScore = 0;
-        if (!scoresList.isEmpty()) {
-            for (int score : scoresList) {
-                if (score > highScore) {
-                    highScore = score;
+        String recordPlayer = "";
+        if (!players.isEmpty()) {
+            for (Player player : players) {
+                if (player.getScore() > highScore) {
+                    highScore = player.getScore();
+                    recordPlayer = player.getName();
                 }
             }
-            System.out.println("The highest score is: " + highScore);
+            System.out.println("The highest score is: " + highScore + " and is held by " +recordPlayer);
         } else {
             System.out.println("No scores in list...");
         }
     }
 
 
-    private static Player createPlayer(ArrayList<Player> playerList) {
+    private static void addPlayer(ArrayList<Player> players) {
         Scanner scanner = new Scanner(System.in);
         Player newPlayer = new Player();
         boolean validInput = false;
@@ -114,6 +116,7 @@ public class Main {
                 validInput = true;
             } catch (InputMismatchException e) {
                 System.out.println("Felaktigt namn, försök igen!");
+                scanner.nextLine();
             }
         }
         validInput = false;
@@ -124,14 +127,16 @@ public class Main {
                 validInput = true;
             } catch (InputMismatchException e) {
                 System.out.println("Felaktig poäng, försök igen!");
+                scanner.nextLine();
             }
         }
+        players.add(newPlayer);
         scanner.nextLine();
-        return newPlayer;
+
     }
 
     private static void printMenu(int scoreCount, String[] menuItems) {
-        System.out.println("\n*** Total number of scores in list: " + scoreCount + " ***\n");
+        System.out.println("\n*** Total number of players: " + scoreCount + " ***\n");
         for (String item : menuItems) {
             System.out.println(item);
         }
@@ -150,3 +155,4 @@ public class Main {
         return checkBoolean;
     }
 }
+
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,49 @@
-public class Competitor {
+import java.util.UUID;
+
+public class Player {
+    private String name;
+    private int score;
+    private final String id = UUID.randomUUID().toString();
+
+    Player() {
+        name = "Anonymous";
+        score = 0;
+    }
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    Player(String name) {
+        this.name = name;
+        score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+
+    public String toString() {
+        return "***** \nPlayer:" +
+                "\nName: " + name +
+                "\nScore:" + score;
+    }
 }
